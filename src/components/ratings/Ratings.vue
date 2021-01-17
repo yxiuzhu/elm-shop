e1dcca2d00fff1e16a194cfd94c927ff0f9dfb4f
y_xiuzhu_glb@163.com <y_xiuzhu@163.com>
2021-01-17 17:41:22 +0800

    完成Ratings评价页

--- src/components/ratings/Ratings.vue
+++ src/components/ratings/Ratings.vue
@@ -1,15 +1,255 @@
 <template>
-  <div>
-    I am Ratings
+  <div class="ratings" ref="ratings">
+    <!-- rating-content的内容超过外层ratings的高度，则会滚动 -->
+    <div class="rating-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <rating-select @ratingevent="ratingevent"
+                     :select-type="selectType" 
+                     :only-content="onlyContent"
+                     :ratings="ratings"></rating-select>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="(rating, index) in ratings" :key="index" 
+              class="rating-item"
+              v-show="needShow(rating.rateType, rating.text)">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar" alt="">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="(item, index) in rating.recommend" :key="index">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Star from '@/components/star/Star'
+  import Split from '@/components/split/Split'
+  import RatingSelect from '@/components/ratingselect/RatingSelect'
+
+  import BScroll from 'better-scroll'
+
+  const ALL = 2;
+  const ERR_OK = 0;
+
   export default {
     name: 'Ratings',
+    components: {
+      Star,
+      Split,
+      RatingSelect
+    },
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        ratings: [],
+        selectType: ALL,
+        // 评论组件上来即展示
+        onlyContent: true
+      }
+    },
+    created() {
+      const url = '/api/ratings';
+      this.$http.get(url).then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data;
+          // 当数据变了，DOM的位置没变
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.ratings, {
+              click: true
+            });
+          });
+        }
+      });
+    },
+    methods: {
+      ratingevent(type, data) {
+        // data数据中包含selectType和onlyContent
+        console.log(data);
+        this[type] = data;
+      },
+      needShow(type, text) {
+        // 勾选只看有内容的评价，但实际没评论
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      }
+    }
   }
 </script>
 
 <style lang='stylus'>
+  @import "../../common/stylus/mixin.styl"
 
+  .ratings
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      display: flex
+      padding: 18px 0
+      .overview-left
+        flex: 0 0 137px
+        padding: 6px 0
+        width: 137px
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+        text-align: center
+        .score
+          margin-bottom: 6px
+          line-height: 28px
+          font-size: 24px
+          color: rgb(255, 153, 0)
+        .title
+          margin-bottom: 8px
+          line-height: 12px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+        .rank
+          line-height: 10px;
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+      .overview-right
+        // 右侧自适应
+        flex: 1
+        padding: 6px 0 6px 24px
+        .score-wrapper
+          margin-bottom: 8px
+          .title
+            display: inline-block
+            line-height: 18px
+            vertical-align: top
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .star
+            display: inline-block
+            margin: 0 12px
+            vertical-align: top
+          .score
+            display: inline-block
+            line-height: 18px
+            vertical-align: top
+            font-size: 12px
+            color: rgb(255, 153, 0)
+        .delivery-wrapper
+          // 此处不需要inline-block和top是因为两部分内容都是文字默认会对齐
+          .title
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .delivery
+            margin-left: 12px
+            font-size: 12px
+            color: rgb(147, 153, 159)
+    .rating-wrapper
+      padding: 0 18px
+      .rating-item
+        display: flex
+        padding: 18px 0
+        border-1px(rgba(7, 17, 27, 0.1))
+        .avatar
+          flex: 0 0 28px
+          width: 28px
+          margin-right: 12px
+          img
+            border-radius: 50%
+        .content
+          position: relative
+          flex: 1
+          .name
+            margin-bottom: 4px
+            line-height: 12px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+          .star-wrapper
+            margin-bottom: 6px
+            .star
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+            .delivery
+              display: inline-block
+              vertical-align: top
+              line-height: 12px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            font-size: 12px   
+            color: rgb(7, 17, 27)
+          .recommend
+            line-height: 16px
+            .icon-thumb_up, .item
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              color: rgb(0, 160, 220)
+            .item
+              display: inline-block
+              vertical-align: top
+              border: 1px solid rgba(7, 17, 27, 0.1)
+              padding: 0 6px
+              border-radius: 2px
+              color: rgb(147, 153, 159)
+              background: #fff
+          .time
+            position: absolute
+            right: 18px
+            top: 0
+            line-height: 12px
+            font-size: 10px
+            font-weight: 700
+            color: rgb(147, 153, 159)
 </style>
