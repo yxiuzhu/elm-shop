72f6b9e860fda57eafee7c17f9a4c95a638b6dfa
yxiuzhu <y_xiuzhu@163.com>
2021-03-30 21:10:19 +0800

    项目优化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack')
+
 const appData = require('./data.json')
 const seller = appData.seller
 const goods = appData.goods
@@ -28,24 +30,24 @@ module.exports = {
       errors: false
     },
     before(app) {
-      app.get('/api/seller', function(req, res) {
+      app.get('/api/seller', function (req, res) {
         const id = req.query.id
         res.json({
           // 返回0：默认返回的数据是成功的
           errno: 0,
           // 因为mock数据中没有id，这里给数据拼接上id
-          data: Object.assign({}, seller, {id})
+          data: Object.assign({}, seller, { id })
         })
       })
 
-      app.get('/api/goods', function(req, res) {
+      app.get('/api/goods', function (req, res) {
         res.json({
           errno: 0,
           data: goods
         })
       })
 
-      app.get('/api/ratings', function(req, res) {
+      app.get('/api/ratings', function (req, res) {
         res.json({
           errno: 0,
           data: ratings
@@ -67,5 +69,10 @@ module.exports = {
     config
       .plugin('webpack-bundle-analyzer')
       .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+
+    config
+      .plugin('context')
+      .use(webpack.ContextReplacementPlugin,
+        [/moment[/\\]locale$/, /zh-cn/])
   }
 }
